3ba4d0147b03b9a6afc253ebb45f19bb65970ea5
anandan <anandan@mail.uc.edu>
2021-08-24 17:21:45 -0400

    Updated hi cs script 1
    base game initial push

--- Assets/hi.cs
+++ Assets/hi.cs
@@ -7,7 +7,7 @@ public class hi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test comment
     }
 
     // Update is called once per frame
